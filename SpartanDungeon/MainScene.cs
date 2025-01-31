ef1c4e9e427e9eaf6393dd70aa919e19976edfbe
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-01-31 19:39:28 +0900

    MainMenu cs 추가

--- SpartanDungeon/MainScene.cs
+++ SpartanDungeon/MainScene.cs
@@ -5,75 +5,7 @@ namespace SpartanDungeon
 
     internal class MainScene
     {
-
-
-
-
-
-        public class MainMenu
-        {
-            private Player player;
-
-            public MainMenu(Player player)
-            {
-                this.player = player;
-            }
-
-            public void ShowMenu()
-            {
-                while (true)
-                {
-                    foreach (MenuOption option in Enum.GetValues(typeof(MenuOption)))
-                    {
-                        Console.WriteLine($"{(int)option}. {GameData.MenuDescriptions[option]}");
-                    }
-
-                    Console.Write("\n원하시는 행동을 입력해주세요: ");
-                    if (int.TryParse(Console.ReadLine(), out int input) && Enum.IsDefined(typeof(MenuOption), input))
-                    {
-                        MenuOption choice = (MenuOption)input;
-
-
-                        switch (choice)
-                        {
-                            case MenuOption.Status:
-                                ShowStatus();
-                                break;
-                            case MenuOption.Inventory:
-                                ShowInventory();
-                                break;
-                            case MenuOption.Shop:
-                                ShowShop();
-                                break;
-                            case MenuOption.Exit:
-                                Console.WriteLine("게임을 종료합니다.");
-                                return;
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("잘못된 입력입니다.");
-                    }
-                }
-            }
-
-            private void ShowStatus()
-            {
-                Console.WriteLine($"\n[상태 보기]\n이름: {player.name}  ({GameData.JobDescriptions[player.job]})\n레벨: {player.level}\n" +
-                    $"공격력: {player.offensive}\n방어력: {player.defensive}\n체력: {player.curHp} / {player.maxHp}\n" +
-                    $"골드: {player.gold} G");
-            }
-
-            private void ShowInventory()
-            {
-                Console.WriteLine("\n[인벤토리]\n현재 아이템이 없습니다.\n");
-            }
-
-            private void ShowShop()
-            {
-                Console.WriteLine("\n[상점]\n아직 상점이 준비되지 않았습니다.\n");
-            }
-        }
+       
 
         static void Main(string[] args)
         {
